a9c53b7a9cc6e56e6f88629e65b8c17eb3251d3d
Nildip <qanildip@gmail.com>
2020-04-29 21:31:51 -0400

    Initial commit

---
+++ Locators/locators.py
@@ -0,0 +1,36 @@
+__author__ = 'Nildip Patel'
+
+
+# This class contains all the locators for website.
+
+class Locators():
+    # header page objects
+    mailto_link = ""
+    login_btn = ""
+    registration_btn = ""
+
+    # footer page objects
+    pricing_link = ""
+    aleri_community_link = ""
+    aboutus_link = ""
+    career_link = ""
+    blog_link = ""
+    contact_link = ""
+    login_link = ""
+    getstarted_link = ""
+    termsofservice_link = ""
+    privacypolicy_link = ""
+    security_link = ""
+
+    # login page objects
+    firm_name = ""
+
+    # home page objects
+    getstarted_btn = ""
+    book_demo_btn = ""
+    dark_mode_switch = ""
+
+    # support page
+    enter_email_txtbx = ""
+    enter_msg_txtbx = ""
+    send_msg_btn = ""
