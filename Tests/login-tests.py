ae4e58bcaf1a8511820e79895b5bb0374eacc619
Nildip <qanildip@gmail.com>
2020-05-08 17:27:16 -0400

    first test success

--- Tests/login-tests.py
+++ Tests/login-tests.py
@@ -1,16 +1,22 @@
 # __author__ = 'Nildip Patel'
 
+import allure
 import pytest
 
 
+#@allure.step
+@allure.feature('Test - Auomation Framework in Python')
+@allure.story('story1')
 @pytest.mark.usefixtures("driver_init")
 class TestExampleOne:
 
+    @allure.title("This test has a custom title")
     def test_title(self):
         """
         Verify title of page
         :return: None
         """
+        allure.attach('A text attacment in module scope fixture', 'blah blah blah', allure.attachment_type.TEXT)
         self.driver.get("https://aleri.ca/")
         assert self.driver.title == "Aleri"
 
